17aae1ec6140b2e27e4c43ad7fad78bb5d78ee60
eswaribala <parameswaribala@gmail.com>
2022-03-23 11:12:39 +0530

    Add project files.

---
+++ FundTransfer/Adapters/FundTransferMessageAdapter.cs
@@ -0,0 +1,19 @@
+﻿using CamundaClient.Dto;
+using CamundaClient.Worker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundTransfer.Adapters
+{
+    [ExternalTaskTopic("fundtransfer")]
+    public class FundTransferMessageAdapter : IExternalTaskAdapter
+    {
+        public void Execute(ExternalTask externalTask, ref Dictionary<string, object> resultVariables)
+        {
+            Console.WriteLine("Customer Id {0}", externalTask.Variables["customerId"].Value);
+        }
+    }
+}
---
+++ FundTransfer/Models/FundTransfer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundTransfer.Models
+{
+    public enum Mode
+    {
+        NEFT,RTGS
+    }
+    public class FundTransfer
+    {
+        public long CustomerId { get; set; }
+        public long FromAccountNo { get; set; }
+        public long ToAccountNo { get; set; }
+        public long Amount { get; set; }
+        public string Remarks { get; set; }
+
+        public Mode Mode { get; set; }
+
+
+    }
+}
