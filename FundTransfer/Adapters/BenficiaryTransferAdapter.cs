f0b0b56e59b48b2811762b3ea5d86708a61524a8
eswaribala <parameswaribala@gmail.com>
2022-03-23 15:30:17 +0530

    updated

---
+++ FundTransfer/Adapters/BenficiaryTransferAdapter.cs
@@ -0,0 +1,26 @@
+﻿using CamundaClient.Dto;
+using CamundaClient.Worker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundTransfer.Adapters
+{
+    [ExternalTaskTopic("transfer")]
+    public class BenficiaryTransferAdapter : IExternalTaskAdapter
+    {
+        public void Execute(ExternalTask externalTask, ref Dictionary<string, object> resultVariables)
+        {
+            string status = "failure";
+
+            if (externalTask.Variables["toAccountNo"].Value.ToString().Length == 10)
+            {
+                status = "success";
+            }
+
+            resultVariables.Add("transferstatus", status);
+        }
+    }
+}
