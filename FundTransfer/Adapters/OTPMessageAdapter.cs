9c7c02ee134885d24a20f5ec12e70114a8e87e17
eswaribala <parameswaribala@gmail.com>
2022-03-23 14:34:07 +0530

    updated

---
+++ FundTransfer/Adapters/OTPMessageAdapter.cs
@@ -0,0 +1,25 @@
+﻿using CamundaClient.Dto;
+using CamundaClient.Worker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundTransfer.Adapters
+{
+    [ExternalTaskTopic("otp")]
+    public class OTPMessageAdapter : IExternalTaskAdapter
+    {
+        public void Execute(ExternalTask externalTask, ref Dictionary<string, object> resultVariables)
+        {
+            Boolean status = false;
+           if(externalTask.Variables["otp"].Value.ToString().Length == 4)
+            {
+                status = true;
+            }
+            resultVariables.Add("status", status);
+
+        }
+    }
+}
